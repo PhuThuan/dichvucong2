67d5edebae7607fa93d0d504c68ac6c422f0b67f
KizunaYuuki <time0009000@gmail.com>
2023-06-19 16:14:53 +0700

    Optimize Trang yêu cầu dịch của khách hàng #20

--- resources/js/Components/ComboboxInputs.vue
+++ resources/js/Components/ComboboxInputs.vue
@@ -9,42 +9,16 @@ const props = defineProps({
     }
 });
 
-// Dữ liệu lấy từ Database, mảng field input. http://127.0.0.1:8000/user/service/{id_service}/{id_services_fiedls}
-let service_field_value = ref([
-    {
-        id: 1,
-        services_fields_id: 3,
-        name: 'Java', // Nội dung input radio, select,...
-    },
-    {
-        id: 1,
-        services_fields_id: 3,
-        name: 'C++', // Nội dung input radio, select,...
-    },
-]);
+// Dữ liệu lấy từ Database, mảng field input. http://127.0.0.1:8000/user/service/{id_service}
+let service_field_value = ref(props.services_fields.service_field_value)
 
 // Khởi tạo biến lấy giá trị của đầu vào (input)
 let input = ref(props.services_fields.placeholder)
 
 function updateFormData(attribute, value) {
     // Cập nhật giá trị của trường input trong biến formData của component cha
-    // props.formData[attribute] = value;
-
-    // if (value === props.services_fields.placeholder) {
-    //     Object.defineProperty(props.formData, attribute, {
-    //         value: undefined,
-    //         writable: true, // Cho phép ghi đè giá trị thuộc tính
-    //         enumerable: true, // Có thể lặp qua thuộc tính
-    //         configurable: true // Có thể cấu hình lại thuộc tính
-    //     });
-    // } else
-    //     Object.defineProperty(props.formData, attribute, {
-    //         value: value,
-    //         writable: true, // Cho phép ghi đè giá trị thuộc tính
-    //         enumerable: true, // Có thể lặp qua thuộc tính
-    //         configurable: true // Có thể cấu hình lại thuộc tính
-    //     });
-    // console.log(props.formData);
+    if (input !== props.services_fields.placeholder)
+        props.formData[props.services_fields.field_name] = `select,${input.value}`;
 };
 
 // computed chuyển đổi chuỗi thành đối tượng
@@ -75,8 +49,8 @@ const stringToObject = computed(() => {
                 focus:border-blue-500 block w-full p-2.5" :aria-label="services_fields.label"
             :placeholder="services_fields.placeholder" :="stringToObject"
             @change="updateFormData(services_fields.field_name, input)">
-            <option :value="services_fields.placeholder" selected>{{ services_fields?.placeholder }}</option>
-            <option v-for="(value, index) in service_field_value">{{ value.name }}</option>
+            <option selected>{{ services_fields?.placeholder }}</option>
+            <option :value="value.id" v-for="(value, index) in service_field_value">{{ value.name }}</option>
         </select>
     </div>
 </template>
