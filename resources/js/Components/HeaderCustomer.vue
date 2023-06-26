9bdf133a5b339025252a9c68a2aaf60a8ba9a8db
KizunaYuuki <time0009000@gmail.com>
2023-06-26 13:40:02 +0700

    #49, #50 Fix header

--- resources/js/Components/HeaderCustomer.vue
+++ resources/js/Components/HeaderCustomer.vue
@@ -1,35 +1,18 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
-
-defineProps({
-    canLogin: {
-        type: Boolean,
-    },
-    canRegister: {
-        type: Boolean,
-    },
-    laravelVersion: {
-        type: String,
-        required: true,
-    },
-    phpVersion: {
-        type: String,
-        required: true,
-    },
-});
 </script>
 <template>
-    <header class="sticky top-0 " style="background-color: #37b6ff;">
+    <header class="sticky top-0 bg-[#37b6ff]">
         <div class="flex text-center font-semibold flex-wrap content-center h-16">
-            <Link  :href="route('dashboard')" class="flex-1 w-32 b">
-                Trang chủ
+            <Link :href="route('dashboard')" class="flex-auto">
+            Trang chủ
             </Link>
-            <Link :href="route('contact')" class="flex-1 w-32">
-                Tổng đài hỗ trợ
+            <Link :href="route('contact')" class="flex-auto">
+            Tổng đài hỗ trợ
             </Link>
-            <Link href="/profile" v-if="$page.props.auth.user" class="flex-1 w-32">Tài khoản</Link>
-            <Link href="/login" v-else class="flex-1 w-32">
-                Đăng nhập
+            <Link href="/user/profile/service" v-if="$page.props.auth.user" class="flex-auto">Tài khoản</Link>
+            <Link href="/login" v-else class="flex-auto">
+            Đăng nhập
             </Link>
         </div>
     </header>
