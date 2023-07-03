292ccf49a6fbea40f7f8bf72dc00609a9888e9ee
ShikiUrara <phub1906744@student.ctu.edu.vn>
2023-07-03 11:36:17 +0700

    #106 Add thêm trang cài đặt VueJS

---
+++ resources/js/Pages/SettingEmail.vue
@@ -0,0 +1,64 @@
+<script setup>
+import Home_Admin from '@/Components/Home_Admin.vue';
+import { defineProps } from 'vue';
+defineProps({ settingEmail: Array });
+import { Head, Link, useForm } from '@inertiajs/vue3';
+//let settingEmail=[{MAIL_USERNAME:"fsdfsdfas@mail.com",MAIL_ENCRYPTION:"tls",MAIL_FROM_NAME:"Dichvu",MAIL_PORT:'123',MAIL_HOST:'smtp@email.com'}]
+const submit = () => {
+    //console.log(form);
+        form.post(route('mail_setting'), {
+            onFinish: () => alert,
+        });
+    
+};
+
+const form = useForm({
+    // settingEmail: [{MAIL_USERNAME:"",MAIL_ENCRYPTION:"",MAIL_FROM_NAME:"",MAIL_PORT:'',MAIL_HOST:''}]
+    MAIL_USERNAME: settingEmail[0].MAIL_USERNAME,
+    MAIL_ENCRYPTION: settingEmail[0].MAIL_ENCRYPTION,
+    MAIL_FROM_NAME: settingEmail[0].MAIL_FROM_NAME,
+    MAIL_PORT: settingEmail[0].MAIL_PORT,
+    MAIL_HOST: settingEmail[0].MAIL_HOST,
+    MAIL_PASSWORD: ""
+});
+</script>
+<template>
+    <Head title="Setting Email"/>
+    <Home_Admin>
+        <div class="my-3 text-lg">
+            Cấu hình email
+        </div>
+       
+<form @submit.prevent="submit">
+    <div class="grid gap-6 mb-6 md:grid-cols-2">
+        <div>
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Mail User Name </label>
+            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5      " placeholder="exmaple@email.com" required v-model="form.MAIL_USERNAME">
+        </div>
+        <div>
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Mail Password</label>
+            <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5      " placeholder="•••••••••" required v-model="form.MAIL_PASSWORD">
+        </div>
+        <div>
+            <label for="encryption" class="block mb-2 text-sm font-medium text-gray-900 ">Mail Encryption</label>
+            <input type="text" id="encryption" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5      " placeholder="Mail Encryption" required v-model="settingEmail[0].MAIL_ENCRYPTION">
+        </div>  
+        <div>
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">Mail From Name</label>
+            <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5      " placeholder="Mail Name"  required v-model="settingEmail[0].MAIL_FROM_NAME">
+        </div>
+        <div>
+            <label for="port" class="block mb-2 text-sm font-medium text-gray-900 ">Mail Port</label>
+            <input type="text" id="port" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5      " placeholder="456" required v-model="settingEmail[0].MAIL_PORT">
+        </div>
+        <div>
+            <label for="host" class="block mb-2 text-sm font-medium text-gray-900 ">Mail Host</label>
+            <input type="text" id="host" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5      " placeholder="smtp.gmail.comx" required v-model="settingEmail[0].MAIL_HOST">
+        </div>
+    </div>
+    
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center   ">Submit</button>
+</form>
+
+    </Home_Admin>
+</template>
